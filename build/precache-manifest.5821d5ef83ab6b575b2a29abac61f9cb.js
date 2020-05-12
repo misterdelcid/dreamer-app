0703110df4f88e30e78ffe8210122f53a38d4c53
Christian <Christianwdelcid@gmail.com>
2020-05-11 21:46:40 -0400

    Adding building build folder

---
+++ build/precache-manifest.5821d5ef83ab6b575b2a29abac61f9cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3608456f4da4ac5aefb3dabc5be1a373",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd95906a8c82976b861a",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "0a052ec1a87bad2ca2c2",
+    "url": "/static/js/2.c7806e1a.chunk.js"
+  },
+  {
+    "revision": "dcd1d80362aef046a5450470cca1218a",
+    "url": "/static/js/2.c7806e1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd95906a8c82976b861a",
+    "url": "/static/js/main.eb0429fe.chunk.js"
+  },
+  {
+    "revision": "339df296f620abc5dada",
+    "url": "/static/js/runtime-main.8bdd6d58.js"
+  }
+]);
