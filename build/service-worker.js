90b252f5eff8dc2b9a7813fbec0b239e958468f5
Christian <Christianwdelcid@gmail.com>
2020-05-12 21:21:36 -0400

    Added Hashrouter for GitHub Pages

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5821d5ef83ab6b575b2a29abac61f9cb.js"
+  "/dreamer-app/precache-manifest.dd5f5c95c67b7ca0aaef5dd83d2dcaaa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dreamer-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux'
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom" 
+import { HashRouter as Router, Switch, Route } from "react-router-dom" 
 import { AddPostPage, EditPostPage, SearchPostsPage, PageNotFound, NavBar, HomePage, TitleBar } from './components'
 import styled, { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from './styles/theme'
